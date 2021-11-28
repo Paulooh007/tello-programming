38ad3476728973b713f1e76a2a31b19e101f430e
paul <okewunmipaul@yahoo.com>
2021-11-28 16:55:39 +0100

    stream drone video feed

--- basic-movements.py
+++ basic-movements.py
@@ -5,3 +5,19 @@ me = tello.Tello()
 me.connect()
 
 print(me.get_battery())
+
+#
+me.takeoff()
+me.send_rc_control(0,5 ,0,0)
+sleep(2)
+me.send_rc_control(20,0,0,0)
+sleep(2)
+me.send_rc_control(0,0,0,0)
+me.flip_back()
+sleep(2)
+me.flip("right")
+sleep(2)
+me.send_rc_control(0,0,0,0)
+
+
+me.land()
